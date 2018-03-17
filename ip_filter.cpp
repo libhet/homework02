1791a4d72ff5fd13c182663439d169cbf627909c
Denis Shevchenko <cookie-monstr@live.com>
2018-03-17 22:38:48 +0300

    functions split

--- ip_filter.cpp
+++ ip_filter.cpp
@@ -3,11 +3,13 @@
 #include <iostream>
 #include <string>
 #include <vector>
-
 // = = = = = = = = = =
 #include <fstream>
 #include <algorithm>
 #include <iterator>
+#include <list>
+#include <tuple>
+#include <functional>
 
 // ("",  '.') -> [""]
 // ("11", '.') -> ["11"]
@@ -34,17 +36,107 @@ std::vector<std::string> split(const std::string &str, char d)
     return r;
 }
 
-std::ostream& operator<<(std::ostream& s, const std::vector<std::string>& vs)
+namespace CHECK
 {
-    return s << vs.at(0) << "." << vs.at(1) << "." << vs.at(2) << "." << vs.at(3);
+    struct No {};
+    template<typename... Args>
+    No emplace_back(Args&& ...args);
+
+    template<typename T, typename Arg = T>
+    struct EmplaceBackExists
+    {
+        enum { value = !std::is_same<decltype(*(T*)(0) == *(Arg*)(0)), No>::value };
+    };
+}
+
+template<typename ValueType, template<typename T = ValueType> class ContainerType>
+ContainerType<ValueType> split(const std::string &str, char d) {
+    ContainerType<ValueType> r;
+    static_assert(CHECK::EmplaceBackExists<ContainerType<ValueType>>::value, "Container doesn't have emplace_back()");
+
+    std::string::size_type start = 0;
+    std::string::size_type stop = str.find_first_of(d);
+    while(stop != std::string::npos)
+    {
+        r.emplace_back(str.substr(start, stop - start));
+
+        start = stop + 1;
+        stop = str.find_first_of(d, start);
+    }
+
+    r.emplace_back(str.substr(start));
+
+    return r;
+};
+
+template<typename ValueType>
+std::tuple<ValueType> split<ValueType, std::tuple<ValueType>>(const std::string &str, char d) {
+    std::tuple<std::common_type<ValueType>> r;
+    //Создать приведение всех типов к одному внутри тапла. Использовать размер ValueTypes
+    std::string::size_type start = 0;
+    std::string::size_type stop = str.find_first_of(d);
+    while(stop != std::string::npos)
+    {
+        r = std::tuple_cat(r, std::tuple<ValueType>(stoi(str.substr(start, stop - start)))); //Работает только с числами? INT
+    start = stop + 1;
+    stop = str.find_first_of(d, start);
+    }
+
+    r = std::tuple_cat(r, std::tuple<ValueType>(stoi(str.substr(start, stop - start))));
+
+    return r;
+};
+
+
+template<typename ValueType, typename... ValueTypes>
+std::tuple<ValueTypes...> split(const std::string &str, char d) {
+    std::tuple<std::common_type<ValueType>> r;
+//Создать приведение всех типов к одному внутри тапла. Использовать размер ValueTypes
+    std::string::size_type start = 0;
+    std::string::size_type stop = str.find_first_of(d);
+    while(stop != std::string::npos)
+    {
+        r = std::tuple_cat(r, std::tuple<ValueType>(stoi(str.substr(start, stop - start)))); //Работает только с числами? INT
+        start = stop + 1;
+        stop = str.find_first_of(d, start);
+    }
+
+    r = std::tuple_cat(r, std::tuple<ValueType>(stoi(str.substr(start, stop - start))));
+
+    return r;
+}
+
+std::tuple<uint8_t, uint8_t, uint8_t, uint8_t> make_tuple_from_vector_of_string (const std::vector<std::string> &v)
+{
+    return std::tuple<uint8_t, uint8_t, uint8_t, uint8_t>(static_cast<uint8_t>(stoi(v.at(0))),
+                                                          static_cast<uint8_t>(stoi(v.at(1))),
+                                                          static_cast<uint8_t>(stoi(v.at(2))),
+                                                          static_cast<uint8_t>(stoi(v.at(3))));
+};
+
+//std::ostream& operator<<(std::ostream& s, const std::vector<std::string>& vs)
+//{
+//    return s << vs.at(0) << "." << vs.at(1) << "." << vs.at(2) << "." << vs.at(3);
+//}
+
+using ip_type = std::tuple<uint8_t, uint8_t, uint8_t, uint8_t>;
+using ip_container = std::list<ip_type>;
+
+namespace std {
+    std::ostream &operator<<(std::ostream &os, const ip_type &ip) {
+
+        os << std::get<0>(ip) << "."
+           << std::get<1>(ip) << "."
+           << std::get<2>(ip) << "."
+           << std::get<3>(ip);
+        return os;
+    }
 }
 
 int main(int argc, char const *argv[])
 {
-
     try
     {
-
         std::ifstream in("../in.txt");
         std::streambuf *cinbuf = std::cin.rdbuf(); //save old buf
         std::cin.rdbuf(in.rdbuf()); //redirect std::cin to in.txt!
@@ -53,39 +145,39 @@ int main(int argc, char const *argv[])
         std::streambuf * coutbuf = std::cout.rdbuf();
         std::cout.rdbuf(out.rdbuf());
 
-        std::vector<std::vector<std::string> > ip_pool;
-
+        ip_container ip_pool;
 
         for(std::string line; std::getline(std::cin, line);)
         {
             std::vector<std::string> v = split(line, '\t');
-            ip_pool.push_back(split(v.at(0), '.'));
+            ip_pool.emplace_back(split<std::string,std::tuple<std::string>>(v.at(0), '.'));
         }
 
         // TODO reverse lexicographically sort
-        std::sort(ip_pool.begin(),ip_pool.end(),
-                  [](const std::vector<std::string> &a, const std::vector<std::string> &b)
-                  {
-                      uint32_t ip_a = 0, ip_b = 0;
-                      for(int i = 0; i < 4; ++i) {
-                          ip_a |= (unsigned char)std::stoi(a[i]) << (24 - (i*8));
-                          ip_b |= (unsigned char)std::stoi(b[i]) << (24 - (i*8));
-                      }
-                      return ip_a > ip_b;
-                  });
 
-        for(std::vector<std::vector<std::string> >::const_iterator ip = ip_pool.cbegin(); ip != ip_pool.cend(); ++ip)
-        {
-            for(std::vector<std::string>::const_iterator ip_part = ip->cbegin(); ip_part != ip->cend(); ++ip_part)
-            {
-                if (ip_part != ip->cbegin())
-                {
-                    std::cout << ".";
-                }
-                std::cout << *ip_part;
-            }
-            std::cout << std::endl;
-        }
+//        std::sort(std::begin(ip_pool),std::end(ip_pool));//,
+//                  [](const std::vector<std::string> &a, const std::vector<std::string> &b)
+//                  {
+//                      uint32_t ip_a = 0, ip_b = 0;
+//                      for(int i = 0; i < 4; ++i) {
+//                          ip_a |= (unsigned char)std::stoi(a[i]) << (24 - (i*8));
+//                          ip_b |= (unsigned char)std::stoi(b[i]) << (24 - (i*8));
+//                      }
+//                      return ip_a > ip_b;
+//                  });
+
+//        for(std::vector<std::vector<std::string> >::const_iterator ip = ip_pool.cbegin(); ip != ip_pool.cend(); ++ip)
+//        {
+//            for(std::vector<std::string>::const_iterator ip_part = ip->cbegin(); ip_part != ip->cend(); ++ip_part)
+//            {
+//                if (ip_part != ip->cbegin())
+//                {
+//                    std::cout << ".";
+//                }
+//                std::cout << *ip_part;
+//            }
+//            std::cout << std::endl;
+//        }
 
         // 222.173.235.246
         // 222.130.177.64
@@ -97,17 +189,27 @@ int main(int argc, char const *argv[])
 
         // TODO filter by first byte and output
         // ip = filter(1)
-//        auto filter = [&](unsigned char x){
-//            std::copy_if(ip_pool.cbegin(), ip_pool.cend(),
-//                      std::ostream_iterator<std::vector<std::string>>(std::cout, "/n"),
-//                         [&](const std::vector<std::string> & v)
-//                         {
-//                         return (stoi(v.at(0)) == x);
-//                         });
+
+//    std::vector<std::vector<int>>::value_type
+//        auto filter = [&](unsigned char byte){
+//            std::copy_if(std::begin(ip_pool),std::end(ip_pool),
+//                         std::ostream_iterator<decltype(*std::begin(ip_pool))>(std::cout),
+//                         [byte](const ip_type & ip) {
+//                             unsigned char first_byte = (unsigned char)stoi(ip.at(0));
+//                             return (first_byte == byte);
+//                         }
+//            );
 //        };
 //        filter(1);
-        std::cout << "sdads" << std::endl;
-        std::cout << ip_pool.at(1);
+
+//        std::vector<std::vector<int>> a = {{1,2,3,4},{2,2,3,4}};
+//        std::copy_if(std::begin(a),a.end(),std::ostream_iterator<typename std::vector<int>>(std::cout, "\n"),
+//                     [](std::vector<int> g){return g[0]%2 == 0;}
+//        );
+
+       // filter(1);
+//        std::cout << "sdads" << std::endl;
+//        std::cout << ip_pool.at(1);
 
         // 1.231.69.33
         // 1.87.203.225
@@ -174,3 +276,9 @@ int main(int argc, char const *argv[])
     return 0;
 }
 
+namespace std {
+    std::ostream &operator<<(std::ostream &s, const std::vector<int> &vs) {
+        s << vs.at(0) << "." << vs.at(1) << "." << vs.at(2) << "." << vs.at(3);
+        return s;
+    }
+}
